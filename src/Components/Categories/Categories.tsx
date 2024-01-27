b009b6eba66f944ba4b0d049f8970e508a873fa6
Petrar Raul <petrar96raul@gmail.com>
2024-01-27 11:52:49 +0200

    nice effect of progressive drop added to the categories

--- src/Components/Categories/Categories.tsx
+++ src/Components/Categories/Categories.tsx
@@ -6,7 +6,8 @@ const testArr = [
 function Categories() {
   return (
     <menu className="text-[18px] overflow-auto h-full pb-10">
-      <p className="border-y-[1px] py-3 px-4">Autentificare</p>
+      {/* am dat max height dincolo si forteaza asta in sus de nu se mai vede , poate il mutam altundeva in dom */}
+      <p className="border-y-[1px] py-3 px-4 text-black">Autentificare</p>{" "}
       {testArr.map(() => {
         return <Category />;
       })}
--- src/Components/Categories/Category.tsx
+++ src/Components/Categories/Category.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 
 function Category() {
   const [showOnOff, setShowOnOf] = useState<boolean>(false);
+  const [containerHeight, setContainerHeight] = useState<number>(0);
+
+  useEffect(() => {
+    setContainerHeight(showOnOff ? 250 : 0); // Adjust the height as needed
+  }, [showOnOff]);
+
   const showMore = () => {
     setShowOnOf(!showOnOff);
   };
+
   return (
     <li className="border-y-[1px] py-3 px-4 flex flex-col gap-2">
       {/* main container */}
@@ -35,14 +42,42 @@ function Category() {
       </div>
 
       {/* show more container */}
-      {showOnOff && (
-        <div className="text-[15px] pl-4 flex flex-col gap-1">
-          <p>ASasdasdasd</p>
-          <p>ASasdasdasd</p>
-          <p>ASasdasdasd</p>
-          <p>ASasdasdasd</p>
-        </div>
-      )}
+      <div
+        className="pl-4 flex flex-col gap-2 text-[15px]"
+        style={{
+          maxHeight: `${containerHeight}px`,
+          overflow: "auto",
+          transition: "max-height 0.7s ease",
+        }}
+      >
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+        <p>ASasdasdasd</p>
+      </div>
     </li>
   );
 }
